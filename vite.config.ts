0078ab60003beefcd5e5aa5a697bc6e2a07f661d
Wayne Souto <soutowhs@gmail.com>
2025-01-26 15:12:27 -0300

    feat: migrate to shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "build",
-  },
   resolve: {
     alias: {
-      assets: "/src/assets",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
